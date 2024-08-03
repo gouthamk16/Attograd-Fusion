90777212aa5dbf4715d45df1c42ee46ed189b025
gouthamk16 <gouthamrakesh@icloud.com>
2024-08-03 18:31:17 +0530

    Initial comments

--- diffusion.py
+++ diffusion.py
@@ -1,6 +1,13 @@
-## Autoencoder -> Compressing image features to a smaller latent space using KL regularization 
-## Generating the image from the trained latent representation.
+## Conditional denoising Autoencoder -> Compressing image features to a smaller latent space using KL regularization 
+## Generating the image from the trained latent representation - unets?
 ## Unet architecture - 2D conv layers -> time conditional unet (used by the latent diffusion model) - https://arxiv.org/pdf/2112.10752
-
+"""
+1. Skip connections
+2. Concat layers
+3. Cross Attention
+"""
+# transformer? (unet)
 
 import torch
+
+
